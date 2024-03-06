d0c7bcac97efbc2cc08aff7657296f032232ad91
Eliabe Gai <eliabegai@gmail.com>
2024-03-05 22:58:56 -0300

    feat: finalizado components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,81 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
+'use client'
+
+import { Avatar, AvatarBorderVariants, AvatarVariants } from "@/components/ui/avatar";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { generateUsers } from "@/lib/data";
+import { useState } from "react";
+
+//[&>div]:-ml-2 ml-2 [&>div]:mt-3 -mt-3
 
 export default function Home() {
+  
+  const [size, setSize] = useState<AvatarVariants['size']>('default')
+  const [borderColor, setBorderColor] = useState<AvatarBorderVariants['borderColor']>('primary')
+  const [qtdUsers, setQtdUsers] = useState(20)
+  
+  const users = generateUsers(qtdUsers)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-      </Avatar>
+    <main className="flex container w-full m-auto min-h-screen flex-col bg-purple-900 p-6">
+
+      <div className="mb-8 p-2 flex gap-4 w-42 flex-col sm:flex-row sm:w-full">
+        <div className="w-full">
+          <span className="text-slate-400 text-xs ml-2">Size</span>
+          <Select onValueChange={(size) => setSize(size as AvatarVariants['size'])}>
+            <SelectTrigger className="">
+              <SelectValue placeholder="Size" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="default">Default</SelectItem>
+              <SelectItem value="sm">Small</SelectItem>
+              <SelectItem value="lg">Large</SelectItem>
+              <SelectItem value="xl">Extra Large</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="w-full">
+          <span className="text-slate-400 text-xs ml-2">Quantidade Avatar</span>
+          <Select onValueChange={(qtd) => setQtdUsers(+qtd)}>
+            <SelectTrigger className="">
+              <SelectValue placeholder="Quantidade Avatar" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="20">20</SelectItem>
+              <SelectItem value="50">50</SelectItem>
+              <SelectItem value="100">100</SelectItem>
+              <SelectItem value="200">200</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="w-full">
+          <span className="text-slate-400 text-xs ml-2">Borda Style</span>
+          <Select onValueChange={(borderColor) => setBorderColor(borderColor as AvatarBorderVariants['borderColor'])}>
+            <SelectTrigger className="">
+              <SelectValue placeholder="Borda" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="primary">Primary</SelectItem>
+              <SelectItem value="none">None</SelectItem>
+              <SelectItem value="secondary">Secondary</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+
+      </div>
+      <div className="w-full flex flex-wrap justify-start [&>div]:-ml-2 ml-2 [&>div]:mt-3 -mt-3">
+          {users &&
+            users.map((user, index) => (
+              <Avatar
+                key={index}
+                imgUrl={user.imgUrl}
+                userName={user.userName}
+                fallbackText={user.fallbackText}
+                size={size}
+                borderColor={borderColor}
+              />
+            ))
+          }
+      </div>
     </main>
   );
 }
--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -4,8 +4,9 @@ import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
+import { VariantProps, cva } from "class-variance-authority"
 
-const Avatar = React.forwardRef<
+const AvatarRoot = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
 >(({ className, ...props }, ref) => (
@@ -18,7 +19,7 @@ const Avatar = React.forwardRef<
     {...props}
   />
 ))
-Avatar.displayName = AvatarPrimitive.Root.displayName
+AvatarRoot.displayName = AvatarPrimitive.Root.displayName
 
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
@@ -47,4 +48,60 @@ const AvatarFallback = React.forwardRef<
 ))
 AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName
 
-export { Avatar, AvatarImage, AvatarFallback }
+const avatarRootVariants = cva("", {
+  variants: {
+    size: {
+      default: "size-10",
+      sm:"size-8",
+      lg:"size-20",
+      xl:"size-24"
+    }
+  },
+  defaultVariants: {
+    size: "default"
+  }
+})
+
+const avatarBorderVariants = cva("absolute rounded-full bg-gradient-to-r from-[#78d993] to-[#e087ff]", {
+  variants: {
+    size: {
+      default: "-inset-0.5",
+      sm:"-inset-0.5",
+      lg:"-inset-1",
+      xl:"-inset-1"
+    },
+    borderColor: {
+      none: "inset-0",
+      primary:"from-[#78d993] to-[#e087ff] animate-spin",
+      secondary:"from-blue-500 via-sky-700 to-sky-200 animate-none",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+    borderColor: 'primary'
+  }
+})
+
+type AvatarVariants = VariantProps< typeof avatarRootVariants>
+type AvatarBorderVariants = VariantProps< typeof avatarBorderVariants>
+
+interface AvatarProps {
+  imgUrl: string
+  userName: string
+  fallbackText: string
+}
+
+const Avatar = ({ imgUrl, userName, fallbackText, size, borderColor }: AvatarProps & AvatarVariants & AvatarBorderVariants) => {
+  return (
+    <div className="relative inline-block">
+      <div className={cn(avatarBorderVariants({size, borderColor}))}/>
+      <AvatarRoot className={cn(avatarRootVariants({ size }))}>
+        <AvatarImage src={imgUrl} alt={`Avatar for ${userName}`} className="bg-transparent border border-purple-900 rounded-full" />
+        <AvatarFallback className="bg-transparent border border-purple-900 rounded-full">{fallbackText}</AvatarFallback>
+      </AvatarRoot>
+    </div>
+  )
+}
+
+export { AvatarRoot, AvatarImage, AvatarFallback, Avatar }
+export type { AvatarVariants, AvatarBorderVariants}
